8fff5e40a16f1a406e8d13b0396aafd827d12d93
Angelo Mandara <angelomandara@outlook.it>
2023-01-02 17:27:49 +0100

    Added CommandLine_folder and update main.ipynb
    Nella cartella Commandline_folder c'e il file bash e l'output in formato txt

---
+++ CommandLine_folder/CommandLine.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+echo "We're going to save the ordered couple in a new file txt" 
+awk -F, '{if ($2<$1) print $2 "\t\t\t-" $1; else print $1 "\t\t-" $2}' hero_clear1.csv > heroes_ordered.txt
+
+echo "Let's have a look"
+awk -F, '{print $0}' heroes_ordered.txt | head -n 10
+
+echo "Now let's display the 20 most popular pair of heroes" 
+sed -r 's/\s+//g' heroes_ordered.txt | sort | uniq -c | sort -rnk1 | head -n  20
+
+echo "Number of comics per hero."
+awk -F, '{count[$1]++}END{for(j in count) print count[j] " : " j |"sort -t: -k2r"}' edges_clear.csv | sort -nr | head -n 20
+
+
+echo "Now we create a new txt file where in each line there will be the number of different heroes appearing in the comic, then later we will average "
+echo " So finally this is the average number of heroes in comics."
+awk -F, '{count[$2]++}END{for(j in count) print count[j] |"sort -t: -k2r"}' edges_clear.csv | sort -nr > number_of_heroes_comics.txt
+awk -F, '{ total += $0 } END { print total/NR }' number_of_heroes_comics.txt
